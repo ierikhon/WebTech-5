57281a2c274592a782a874b2c333e4a9caf599a9
ierikhon <kinglordlear@gmail.com>
2018-09-22 17:28:38 +0300

    All this tetris belongs to me

--- Tetris/src/figure.js
+++ Tetris/src/figure.js
@@ -1,48 +1,64 @@
 class Figure {
-    constructor(arr) {
-        this.arr = arr;
+    constructor(matr1, matr2, matr3, matr4) {
+        this.color = '#00FF00';
+        this.positions = [matr1, matr2, matr3, matr4];
         this.x = 0;
         this.y = 0;
-        this.current = 0;
-        this.color = "#FF0000"
-    }
-    rotate(){
-        this.current = ++this.current % 4;
-    }
-    rotateBack(){
-        this.current = --this.current % 4;
-    }
-    makeMatr(){
-        let bin = this.arr[this.current].toString(2);
-        let res = [];
-        while (bin.length < 16){
-            bin = '0' + bin;
-        }
-        for (let i = 0; i < 16; i+=4){
-            res.push(bin.substring(i, i+4).split("").reverse());
-            for (let j = 0; j < 4; j++)
-                res[i/4][j] = parseInt(res[i/4][j]);
-        }
-        return res;
+        this.matrixID = 0;
     }
 
-    static makeFigure(number) {
-        switch (number) {
-            case 0:
-                return new Figure([0x2222, 0x0F00, 0x4444, 0x00F0]);
-            case 1:
-                return new Figure([0x2230, 0x1700, 0x6220, 0x0740]);
-            case 2:
-                return new Figure([0x2260, 0x0710, 0x3220, 0x4700]);
-            case 3:
-                return new Figure([0x3300, 0x3300, 0x3300, 0x3300]);
-            case 4:
-                return new Figure([0x0630, 0x1320, 0x6300, 0x2640]);
-            case 5:
-                return new Figure([0x0720, 0x2320, 0x2700, 0x2620]);
-            case 6:
-                return new Figure([0x0360, 0x2310, 0x3600, 0x4620]);
-        }
+    rotate(side)
+    {
+        if (side === 'Left')
+            this.matrixID = --this.matrixID % 4;
+        else this.matrixID = ++this.matrixID % 4;
+    }
+
+    get matrix ()
+    {
+        return this.positions[this.matrixID];
+    }
+
+    static makeFigure ()
+    {
+       let figureType =  Math.floor(Math.random() * 7);
+       switch (figureType)
+       {
+           case 0:
+               return new Figure([[1,1,0,0],[1,1,0,0],[0,0,0,0],[0,0,0,0]],
+                                 [[1,1,0,0],[1,1,0,0],[0,0,0,0],[0,0,0,0]],
+                                 [[1,1,0,0],[1,1,0,0],[0,0,0,0],[0,0,0,0]],
+                                 [[1,1,0,0],[1,1,0,0],[0,0,0,0],[0,0,0,0]]);
+           case 1:
+               return new Figure([[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]],
+                                 [[0,0,1,0],[0,0,1,0],[0,0,1,0],[0,0,1,0]],
+                                 [[0,0,0,0],[0,0,0,0],[1,1,1,1],[0,0,0,0]],
+                                 [[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]]);
+           case 2:
+               return new Figure([[0,1,0,0],[0,1,0,0],[1,1,0,0],[0,0,0,0]],
+                                 [[1,0,0,0],[1,1,1,0],[0,0,0,0],[0,0,0,0]],
+                                 [[0,1,1,0],[0,1,0,0],[0,1,0,0],[0,0,0,0]],
+                                 [[0,0,0,0],[1,1,1,0],[0,0,1,0],[0,0,0,0]]);
+           case 3:
+               return new Figure([[0,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,0,0]],
+                                 [[0,0,0,0],[1,1,1,0],[1,0,0,0],[0,0,0,0]],
+                                 [[1,1,0,0],[0,1,0,0],[0,1,0,0],[0,0,0,0]],
+                                 [[0,0,1,0],[1,1,1,0],[0,0,0,0],[0,0,0,0]]);
+           case 4:
+               return new Figure([[0,0,0,0],[0,1,1,0],[1,1,0,0],[0,0,0,0]],
+                                 [[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,0,0,0]],
+                                 [[0,1,1,0],[1,1,0,0],[0,0,0,0],[0,0,0,0]],
+                                 [[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,0,0]]);
+           case 5:
+               return new Figure([[0,0,0,0],[1,1,1,0],[0,1,0,0],[0,0,0,0]],
+                                 [[0,1,0,0],[1,1,0,0],[0,1,0,0],[0,0,0,0]],
+                                 [[0,1,0,0],[1,1,1,0],[0,0,0,0],[0,0,0,0]],
+                                 [[0,1,0,0],[0,1,1,0],[0,1,0,0],[0,0,0,0]]);
+           case 6:
+               return new Figure([[0,0,0,0],[1,1,0,0],[0,1,1,0],[0,0,0,0]],
+                                 [[0,1,0,0],[1,1,0,0],[1,0,0,0],[0,0,0,0]],
+                                 [[1,1,0,0],[0,1,1,0],[0,0,0,0],[0,0,0,0]],
+                                 [[0,0,1,0],[0,1,1,0],[0,1,0,0],[0,0,0,0]]);
+       }
     }
 }
-
--- Tetris/src/tetris.js
+++ Tetris/src/tetris.js
@@ -1,25 +1,174 @@
-var columns = 10, rows = 20; //Size of tetris
-var score = 0; //Score
-var record = 0; //Record
-var lines = 0; //Lines
-var lines_level = 0; //Lines to next level
-var level = 0; //Current level
-var board = []; //Board
-var figure = null; //Current figure
-var next_figure = null; //Next figure
-var canvas = null; //Canvas
-var next_canvas = null; //Next figure canvas
-var render_interval = null;
-var user_name = null;
+var fieldWidth = 10;
+var fieldHeight = 20;
 
-function updateInfo(){
-    user_name = localStorage["tetris.username"];
-    document.getElementById("info").innerHTML = "Login: " + user_name +
-        "<br> Lines: " + lines + "<br>Level: " + level + "<br>Score: " + score +
-        "<br> Record: " + record;
+var tetrisF;
+var nextF;
+
+var currentFigure;
+var nextFigure;
+
+var board = [];
+
+var render_interval;
+
+var deletedLines;
+var multiplier;
+var score;
+var level;
+
+
+function startGame()
+{
+    deletedLines = 0;
+    multiplier = 1;
+    score = 0;
+    level = 1;
+    tetrisF = new Canvas(fieldWidth, fieldHeight, "tetrisfield");
+    nextF = new Canvas (4, 4, "nextfigure");
+
+    for (let i=0; i<fieldWidth; i++)
+    {
+        board[i] = [];
+        for (let j=0; j<fieldHeight; j++)
+            board[i][j] = 0;
+    }
+
+    generateFigure();
+    startRender(1000);
 }
 
-function keyListener(event) {
+function startRender(speed)
+{
+    if (speed <= 0)
+        speed = 100;
+    if (render_interval)
+        clearInterval(render_interval);
+    render_interval = setInterval(() => {moveFigure(0, 1)}, speed);
+}
+
+function gameOver()
+{
+    localStorage["tetris.score"] = score;
+    document.location.href = '../web/records.html';
+}
+
+function generateFigure()
+{
+    if (!nextFigure)
+        nextFigure = Figure.makeFigure();
+    currentFigure = nextFigure;
+    currentFigure.x = 5;
+    nextFigure = Figure.makeFigure();
+    nextF.resetAll();
+    nextF.drawFigure(nextFigure);
+
+    if (!checkState())
+        gameOver();
+}
+
+function checkState() {
+    let valid = true;
+    for (let i = 0; i < 4; i++)
+        for (let j = 0; j < 4; j++) {
+            if (currentFigure.matrix[i][j] === 1) {
+                valid = valid && (currentFigure.x + i >= 0) && (currentFigure.x + i < fieldWidth);
+                valid = valid && (currentFigure.y + j >= 0) && (currentFigure.y + j < fieldHeight);
+                valid = valid && (!board[currentFigure.x + i][currentFigure.y + j]);
+                if (!valid)
+                    break;
+            }
+        }
+    return valid;
+}
+
+function moveFigure(offsetX, offsetY)
+{
+    currentFigure.x += offsetX;
+    currentFigure.y += offsetY;
+
+    let valid = checkState();
+    if (!valid)
+    {
+        currentFigure.x -= offsetX;
+        currentFigure.y -= offsetY;
+        if ((offsetX === 0) && (offsetY === 1))
+        {
+            saveFigure();
+            generateFigure();
+        }
+    }
+    update();
+}
+
+    function checkLines()
+    {
+        let deleteNeeded = false;
+        for (let i=0; i<fieldHeight; i++)
+        {
+            let deleteThis = true;
+            for (let j=0; j<fieldWidth; j++)
+            {
+                deleteThis = deleteThis && board[j][i];
+                if (!deleteThis)
+                    break;
+            }
+            if (deleteThis)
+            {
+                deleteNeeded = true;
+                for (let j=0; j<fieldWidth; j++)
+                {
+                    board[j].splice(i, 1);
+                    board[j].unshift(0);
+                }
+                break;
+            }
+        }
+
+        if (deleteNeeded)
+        {
+            deletedLines++;
+            score += 100*multiplier;
+            multiplier++;
+            if (score >= 1000*level)
+            {
+                level++;
+                startRender(1000 / 1.1**(level-1));
+            }
+            checkLines();
+        }
+        multiplier = 1;
+    }
+
+function update()
+{
+    checkLines();
+    tetrisF.resetAll();
+    tetrisF.drawBoard(board);
+    tetrisF.drawFigure(currentFigure);
+    document.getElementById("info").innerHTML = "Login: " + localStorage["tetris.username"] +
+        "<br> Lines: " + deletedLines + "<br>Level: " + level + "<br>Score: " + score;
+}
+
+
+function rotateFigure()
+{
+    currentFigure.rotate();
+    if (!checkState())
+        currentFigure.rotate('Left');
+    update();
+
+}
+
+function saveFigure()
+{
+    for (let i = 0; i < 4; i++)
+        for (let j = 0; j < 4; j++)
+            if ((currentFigure.matrix[i][j] === 1))
+                board[i + currentFigure.x][j + currentFigure.y] = currentFigure.color;
+}
+
+function keyListener(event)
+{
     const keyName = event.key;
     switch (keyName) {
         case 'ArrowLeft':
@@ -38,182 +187,5 @@ function keyListener(event) {
     }
 }
 
-function moveFigure(x, y) {
-    move = true;
-    matr = figure.makeMatr();
-    for (let i = 0; i < 4; i++){
-        for (let j = 0; j < 4; j++){
-            if ((matr[i][j] === 1)){
-                move = move && (figure.x + i + x >=0) && (figure.x + i + x < columns);
-                move = move && (figure.y + j + y >=0) && (figure.y + j + y < rows);
-                move = move && (!board[figure.x + i + x][figure.y + j + y]);
-                if (!move)
-                    break;
-            }
-        }
-    }
-    if (move){
-        figure.x += x;
-        figure.y += y;
-    }
-    if ((!move) && (x === 0) && (y === 1)){
-        saveFigure();
-        figure = newFigure();
-    }
-    updateAll();
-}
-
-function rotateFigure() {
-    figure.rotate();
-    matr = figure.makeMatr();
-    rotate = true;
-    for (let i = 0; i < 4; i++){
-        for (let j = 0; j < 4; j++){
-            if ((matr[i][j] === 1)){
-                rotate = rotate && (figure.x + i >=0) && (figure.x + i < columns);
-                rotate = rotate && (figure.y + j >=0) && (figure.y + j < rows);
-                rotate = rotate && (!board[figure.x + i][figure.y + j]);
-                if (!rotate)
-                    break;
-            }
-        }
-    }
-    if (!rotate)
-        figure.rotateBack();
-    updateAll();
-}
-
-function saveFigure() {
-    for (let i = 0; i < 4; i++){
-        for (let j = 0; j < 4; j++){
-            if ((matr[i][j] === 1)){
-                board[i + figure.x][j + figure.y] = figure.color;
-            }
-        }
-    }
-}
-
-
-function newFigure() {
-    let fig = next_figure;
-    next_figure = Figure.makeFigure(Math.floor(Math.random() * 7));
-    next_figure.color = "#" + Math.random().toString(16).slice(2, 8);
-    next_canvas.render(null, next_figure);
-    if (!fig) {
-        fig = Figure.makeFigure(Math.floor(Math.random() * 7));
-        fig.color = "#" + Math.random().toString(16).slice(2, 8);
-    }
-    fig.x = Math.floor(Math.floor(Math.random() * (columns - 3)));
-    success = true;
-    matr = fig.makeMatr();
-    for (let i = 0; i < 4; i++){
-        for (let j = 0; j < 4; j++){
-            if ((matr[i][j] === 1)){
-                success = success && !board[i + fig.x][j + fig.y];
-            }
-        }
-    }
-    if (!success){
-        gameOver();
-    }
-    return fig;
-}
-
-function updateAll(){
-    checkLines();
-    updateInfo();
-    canvas.render(board, figure);
-}
-
-function checkLines() {
-    remove = false;
-    for (let i = 0; i < rows; i++) {
-        remove_here = true;
-        for (let j = 0; j < columns; j++) {
-            remove_here = remove_here && board[j][i];
-            if (!remove_here)
-                break;
-        }
-        if (remove_here) {
-            remove = true;
-            for (let j = 0; j < columns; j++) {
-                board[j].splice(i, 1);
-                board[j].unshift(0);
-            }
-            break;
-        }
-    }
-    if (remove) {
-        lines++;
-        lines_level++;
-        score = score + level * 100;
-        if (lines_level > 10){
-            lines_level = 0;
-            level++;
-            startRender(1000/level);
-        }
-        if (record < score)
-            record = score;
-        checkLines();
-    }
-}
-
-function init(){
-    canvas = new Canvas(columns, rows, "tetrisfield");
-    next_canvas = new Canvas(4, 4, "nextfigure");
-    document.addEventListener('keydown', keyListener);
-    newGame();
-}
-
-function newGame(){
-    for (let i=0; i<columns; i++){
-        board[i] = [];
-        for (let j=0; j<rows; j++){
-            board[i][j] = 0;
-        }
-    }
-    score = 0;
-    level = 1;
-    lines_level = 0;
-    updateInfo();
-    figure = newFigure();
-    updateAll();
-    startRender(1000 - level*100);
-}
-
-function startRender(speed){
-    if (speed <= 0)
-        speed = 100;
-    if (render_interval)
-        clearInterval(render_interval);
-    render_interval = setInterval(() => {moveFigure(0, 1)}, speed);
-}
-
-function resetRecords(){
-    localStorage.removeItem("tetris.records");
-}
-
-function gameOver(){
-    alert("Game over! Score: " + score);
-    let records = localStorage["tetris.records"];
-    if (!records)
-        records = "Records\n";
-    records = records + user_name + ": " + score + "\n";
-    let recs = records.split("\n");
-    let rec_list = [];
-    for (i=1; i<recs.length-1; i++){
-        let name = recs[i].split(": ")[0];
-        let score = recs[i].split(": ")[1];
-        rec_list.push({name, score});
-    }
-    rec_list.sort((a,b) => b.score - a.score);
-    records = "Records\n";
-    for (i = 0; i < rec_list.length && i < 10; i++){
-        records = records + rec_list[i].name + ": " + rec_list[i].score + "\n";
-    }
-    localStorage["tetris.records"] = records;
-    alert(records);
-    newGame();
-}
-
-init();
+document.addEventListener('keydown', keyListener);
+startGame();
