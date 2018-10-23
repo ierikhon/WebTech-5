612afc639b6e60a53e43a46cabefe33388599790
ierikhon <kinglordlear@gmail.com>
2018-10-23 16:31:22 +0300

    Initial load

---
+++ Auction/gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require("gulp4");
+const less = require("gulp-less");
+const babel = require("gulp-babel");
+const rename = require("gulp-rename");
+const pug = require('gulp-pug');
+const cleanCSS = require("gulp-clean-css");
+const minify = require("gulp-uglify");
+const cleanHTML = require("gulp-minify-html");
+const del = require('del');
+
