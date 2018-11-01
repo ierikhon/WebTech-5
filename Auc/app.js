35e067a709a3c5b977636f88cd66563ef6fd6ff2
ierikhon <kinglordlear@gmail.com>
2018-11-01 09:50:55 +0300

    Games with less. Https configured

--- Auc/app.js
+++ Auc/app.js
@@ -5,6 +5,14 @@ const http = require('http');
 const fs = require('fs');
 const routes = require("./index.js");
 
+let private_key = fs.readFileSync('ssl/yarik.key');
+let certificate = fs.readFileSync('ssl/yarik.csr');
+
+let credentilals = {
+    "key": private_key,
+    "cert": certificate
+};
+
 const server = express();
 
 server.use(bodyParser.json());
@@ -17,7 +25,12 @@ server.use('/css', express.static(__dirname + "/css"));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
+const https_server = https.createServer(credentilals, server);
 const http_server = http.createServer(server);
+
+https_server.listen(443, ()=>{ // Запуск
+    console.log("HTTPS server started at https://localhost:443")
+});
 
 http_server.listen(3000, ()=>{
     console.log('Server started at http://localhost:3000/members');
