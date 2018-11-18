a5f28bff1680f265875c8f082da769f9b806e50c
ierikhon <kinglordlear@gmail.com>
2018-11-18 11:43:58 +0300

    Brokers page start

--- Stock/src/app/brokers/brokers.component.ts
+++ Stock/src/app/brokers/brokers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-brokers',
@@ -10,6 +11,17 @@ export class BrokersComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showdummy() {
+    $('#name_input').val('');
+    $('#price_input').val(0);
+    $('#amm_input').val(10000);
+    $('#modal').show();
+  }
+
+  dismiss_modal() {
+    $('#modal').hide();
   }
 
 }
