5e61c9746b3e6da97774588326e19e9f33da2d37
ierikhon <kinglordlear@gmail.com>
2018-11-27 12:36:31 +0300

    Entities added

--- Game/javascript/Entities.js
+++ Game/javascript/Entities.js
@@ -21,10 +21,65 @@ var Player = Entity.extend({
     move_x: 0,
     move_y: 0,
     draw: function(ctx) {
-        spriteManager.drawSprite(ctx, img, this.pos_x, this.pos_y);
+        spriteManager.drawSprite(ctx, 'player', this.pos_x, this.pos_y);
     },
     update: function() {
 
+    }
+});
+
+var Gold = Entity.extend({
+    size_x: 32,
+    size_y: 32,
+    move_x: 0,
+    move_y: 0,
+    draw: function(ctx) {
+        spriteManager.drawSprite(ctx, 'gold', this.pos_x, this.pos_y);
+    },
+    update: function() {
+
+    }
+});
+
+var Skeleton = Entity.extend({
+    size_x: 32,
+    size_y: 32,
+    move_x: 0,
+    move_y: 0,
+    state: 0,
+    img: 'guard_1',
+    draw: function(ctx) {
+        spriteManager.drawSprite(ctx, this.img, this.pos_x, this.pos_y);
+    },
+    update: function() {
+        this.state = (this.state + 1) % 2;
+        if (this.state === 0)
+            this.img = 'guard_1';
+        if (this.state === 1)
+            this.img = 'guard_2';
+    }
+});
+
+var Bonfire = Entity.extend({
+    size_x: 32,
+    size_y: 32,
+    move_x: 0,
+    move_y: 0,
+    state: 0,
+    img: 'bonfire_1',
+    draw: function(ctx) {
+        spriteManager.drawSprite(ctx, this.img, this.pos_x, this.pos_y);
+    },
+    update: function() {
+        this.state = (this.state + 1) % 4;
+        if (this.state === 0)
+            this.img = 'bonfire_1';
+        if (this.state === 1)
+            this.img = 'bonfire_2';
+        if (this.state === 2)
+            this.img = 'bonfire_3';
+        if (this.state === 3)
+            this.img = 'bonfire_4';
     }
 });
 
